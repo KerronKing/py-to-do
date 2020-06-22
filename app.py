962b42cf76da25473bea48587520b2e0bc8ac0df
KerronKing <king_of_palmiste@hotmail.com>
2020-06-22 13:38:10 -0400

    chore: add app skeleton and db logic

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  content = db.Column(db.String(200), nullable = False)
+  completed = db.Column(db.Integer, default = 0)
+  date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+def __repr__(self):
+  return '<Task %r>' % self.id
+
+@app.route('/', methods = ['POST', 'GET'])
 
 def index():
-  return render_template('index.html')
+  if request.method == 'POST':
+    todo_content = request.form['content']
+    new_content = Todo(content = todo_content)
+
+    try:
+      db.session.add(new_content)
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'There was an issue adding the todo item'
+
+  else:
+    todos = Todo.query.order_by(Todo.date_created).all()
+    return render_template('index.html', todos = todos)
 
 if __name__ == '__main__':
   app.run(debug = True)
